48443ce6764936ffdca701cd6e4156afb5b0867e
Gerardo Delao <Gerardo_delao@ymail.com>
2023-01-06 16:41:58 -0700

    updated about us page

--- src/routes/AboutUs.jsx
+++ src/routes/AboutUs.jsx
@@ -6,20 +6,27 @@ function AboutUs() {
     <>
     <div className={styles.aboutContainer}>
   <div className={styles.mainAboutSectionGrid}>
-    <div class={styles.mainAboutText}>
-        <h2>Meet The Rhodes</h2>
-        <p>Pam Rhodes and David Rhodes have been .</p>
+    <div className={styles.mainAboutText}>
+        <h2>Meet The Gang</h2>
+        <p>Pam Rhodes and David Rhodes.</p>
     </div>  
     <div className={styles.mainAboutPicture}>
+      <img src='Moogie.jpeg' alt='Moogie'></img>
+      <div className={styles.hiddenText}>
+        <h2>Pam Rhodes and David Rhodes.</h2>
+        <div className={styles.hiddenParagraph}>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div>
     </div>
   </div>
 </div>
   <div className={styles.specialtyContainer}>
-    <h2 className={styles.specialtyHeader}>Text will go here</h2>
+    <h2 className={styles.specialtyHeader}>Meet The Rest!</h2>
     <div className={styles.specialtyGrid}>
       <div className={styles.specialtyCard}>
         <div className={styles.image}>
-          <h1>Hello</h1>
+          <img src='Jackson.jpeg' alt='Jackson'></img>
         </div>
          <div className={styles.aboutUs}>
         <h1>Jackson</h1>
@@ -30,7 +37,7 @@ function AboutUs() {
       </div> 
      <div className={styles.specialtyCard}>
         <div className={styles.image}>
-        <h1>Hello</h1>
+        <img src='Benji.jpeg' alt='Benji'></img>
         </div>
          <div className={styles.aboutUs}>
         <h1>Benji</h1>
@@ -41,7 +48,7 @@ function AboutUs() {
       </div> 
      <div className={styles.specialtyCard}>
         <div className={styles.image}>
-          <h1>Hello</h1>
+          <img src='Erin Photo.jpg' alt='Erin'></img>
         </div>
         <div className={styles.aboutUs}>
         <h1>Erin Cranston</h1>
@@ -52,7 +59,7 @@ function AboutUs() {
       </div> 
       <div className={styles.specialtyCard}>
         <div className={styles.image}>
-          <h1>Hello</h1>
+          <img src='https://images.khinsider.com/KINGDOM%20HEARTS%20III/Artwork/Characters/Sora05.png' alt='Callie'></img>
         </div>
         <div className={styles.aboutUs}>
         <h1>Callie Rhodes</h1>
@@ -63,7 +70,7 @@ function AboutUs() {
       </div> 
      <div className={styles.specialtyCard}>
         <div className={styles.image}>
-          <h1>Hello</h1>
+          <img src='Natalie Photo.jpg' alt='Nat'></img>
         </div>
          <div className={styles.aboutUs}>
         <h1>Natalie Cranston</h1>
