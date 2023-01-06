90b3273e182bd4b47a3360d6d6c1c168b112f2a8
Gerardo Delao <Gerardo_delao@ymail.com>
2023-01-05 17:20:50 -0700

    updated about us page

--- src/routes/AboutUs.jsx
+++ src/routes/AboutUs.jsx
@@ -3,18 +3,79 @@ import styles from '../components/stylesheets/AboutUs.module.css'
 
 function AboutUs() {
   return (
+    <>
     <div className={styles.aboutContainer}>
   <div className={styles.mainAboutSectionGrid}>
     <div class={styles.mainAboutText}>
         <h2>Meet The Rhodes</h2>
         <p>Pam Rhodes and David Rhodes have been .</p>
-        {/* <button className="mainAboutButton">About Us</button> */}
     </div>  
     <div className={styles.mainAboutPicture}>
-      
     </div>
   </div>
 </div>
+  <div className={styles.specialtyContainer}>
+    <h2 className={styles.specialtyHeader}>Text will go here</h2>
+    <div className={styles.specialtyGrid}>
+      <div className={styles.specialtyCard}>
+        <div className={styles.image}>
+          <h1>Hello</h1>
+        </div>
+         <div className={styles.aboutUs}>
+        <h1>Jackson</h1>
+         </div>
+        <div className={styles.description}>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div> 
+     <div className={styles.specialtyCard}>
+        <div className={styles.image}>
+        <h1>Hello</h1>
+        </div>
+         <div className={styles.aboutUs}>
+        <h1>Benji</h1>
+         </div>
+        <div className={styles.description}>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div> 
+     <div className={styles.specialtyCard}>
+        <div className={styles.image}>
+          <h1>Hello</h1>
+        </div>
+        <div className={styles.aboutUs}>
+        <h1>Erin Cranston</h1>
+        </div>
+        <div className={styles.description}>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div> 
+      <div className={styles.specialtyCard}>
+        <div className={styles.image}>
+          <h1>Hello</h1>
+        </div>
+        <div className={styles.aboutUs}>
+        <h1>Callie Rhodes</h1>
+        </div>
+        <div className={styles.description}>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div> 
+     <div className={styles.specialtyCard}>
+        <div className={styles.image}>
+          <h1>Hello</h1>
+        </div>
+         <div className={styles.aboutUs}>
+        <h1>Natalie Cranston</h1>
+         </div>
+        <div className={styles.description}>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div> 
+    </div>
+  </div>
+  
+    </>
   )
 }
 
