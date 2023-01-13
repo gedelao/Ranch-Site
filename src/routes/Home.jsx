e8ff62cd8d1fb4253f392c22267b68f14cc684fe
Gerardo Delao <Gerardo_delao@ymail.com>
2023-01-12 17:47:38 -0700

    updating home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,10 +11,20 @@ function Home() {
         </h1>
         </div>
       </div>
-      <div className={styles.box1}>
-      <h1 style={{ display: "grid"  }}>hello</h1>
+      <div className={styles.container}>
+      <div className={styles.boxes}>
+        <div className={styles.boxes1}>
+          <img className={styles.images} src='https://www.humanesociety.org/sites/default/files/2018/06/cat-217679.jpg' alt='' />
+        </div>
+        <div className={styles.boxes1}>
+          <img className={styles.images} src='https://www.humanesociety.org/sites/default/files/2018/06/cat-217679.jpg' alt='' />
+        </div>
+        <div className={styles.boxes1}>
+          <img className={styles.images} src='https://www.humanesociety.org/sites/default/files/2018/06/cat-217679.jpg' alt='' />
+        </div>
       </div>
     </div>
+  </div>
   )
 }
 
